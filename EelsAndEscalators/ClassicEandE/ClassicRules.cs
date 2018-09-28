82ba636855962a0d5f959b318b01be7bdf933545
INIT\HNI <nicohoermann16@gmail.com>
2018-09-28 16:40:43 +0200

    Classic Rules Implemantation Not Finished

--- EelsAndEscalators/ClassicEandE/ClassicRules.cs
+++ EelsAndEscalators/ClassicEandE/ClassicRules.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 using EelsAndEscalators.Configurations;
 using EelsAndEscalators.Contracts;
 
@@ -8,46 +9,100 @@ namespace EelsAndEscalators.ClassicEandE
 {
     public class ClassicRules : IRules
     {
+        private long _idCounter;
+
         private readonly IGame _game;
-        ConfigurationProvider configurationProvider = new ConfigurationProvider();
-        public ClassicRules(IGame game)
+        private readonly IConfigurationProvider _configurationProvider;
+
+        private Dictionary<EntityType, Func<XElement, IEntity>> _entityFactory = new Dictionary<EntityType, Func<XElement, IEntity>>();
+
+        public int numberOfPawns { get; } = 2;
+        public int diceSides { get; } = 6;
+        public int diceResult { get; }
+
+
+        public ClassicRules(IGame game, IConfigurationProvider configurationProvider)
         {
             _game = game;
+            _configurationProvider = configurationProvider;
+
+            _entityFactory = new Dictionary<EntityType, Func<XElement, IEntity>>
+            {
+                { EntityType.Eel, (config) => CreateEel(config)},
+                { EntityType.Escalator, (config) => CreateEscalator(config) },
+            };
         }
 
-        public ClassicRules()
+        public ClassicRules(IGame game)
+             : this(game, new ConfigurationProvider())
+        { }
+
+
+        public void SetupEntitites()
         {
+            _game.Board = new ClassicBoard();
 
+            var configurations = _configurationProvider.GetEntityConfigurations();
+            configurations.ForEach(config =>
+            {
+                var entityType = (EntityType)Convert.ToInt32(config.Element("entitytype"));
+                if (entityType == EntityType.Pawn)
+                    _game.Board.Pawns.Add(CreatePawn(config));
+                else
+                    _game.Board.Entities.Add(_entityFactory[entityType](config));
+            });
         }
-        public int numberOfPawns { get =>numberOfPawns; set =>numberOfPawns = 2; }
-        public int diceSides { get => diceSides; set => diceSides = 6; }
-        public int diceResult { get ;set; }
 
         public IBoard CreateBoard()
         {
             return new ClassicBoard();
         }
 
-        public IPawn CreatePawn(PawnConfig configuraton)
+        public IPawn CreatePawn(XElement configuration)
         {
-            
-            return new ClassicPawn();
+            return new ClassicPawn
+            {
+                color = Convert.ToInt32(configuration.Element("color")),
+                location = Convert.ToInt32(configuration.Element("location")),
+                playerID = Convert.ToInt32(configuration.Element("player_id")),
+                Id = NextId(),
+            };
         }
 
-        public IEntity CreateEel(EelConfig configuration)
+        public IEntity CreateEel(XElement configuration)
         {
-            return new ClassicEel();
+            return new ClassicEel
+            {
+                bottom_location = Convert.ToInt32(configuration.Element("bottom_location")),
+                top_location = Convert.ToInt32(configuration.Element("top_location")),
+                Id = NextId(),
+            };
         }
 
-        public IEntity CreateEscalator(EscalatorConfig configuration)
+        public IEntity CreateEscalator(XElement configuration)
         {
-            return new ClassicEscalator();
+            return new ClassicEscalator
+            {
+                bottom_location = Convert.ToInt32(configuration.Element("bottom_location")),
+                top_location = Convert.ToInt32(configuration.Element("top_location")),
+                Id = NextId(),
+            };
         }
 
         public void RollDice()
         {
 
-            throw new NotImplementedException();
+           
         }
+
+        #region Private methods
+
+        private long NextId()
+        {
+            _idCounter++;
+            return _idCounter;
+        }
+
+        #endregion
     }
 }
