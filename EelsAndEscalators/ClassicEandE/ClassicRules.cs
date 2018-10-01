126f36e348874616b3c2e940667d7791867c00a3
INIT\HNI <nicohoermann16@gmail.com>
2018-10-01 08:50:24 +0200

    using deleted

--- EelsAndEscalators/ClassicEandE/ClassicRules.cs
+++ EelsAndEscalators/ClassicEandE/ClassicRules.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
-using EelsAndEscalators.Configurations;
+
 using EelsAndEscalators.Contracts;
 
 namespace EelsAndEscalators.ClassicEandE
