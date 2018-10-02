771a4c94f9d909517f84dd9f01afaff7ca375ae6
INIT\HNI <nicohoermann16@gmail.com>
2018-10-02 08:54:24 +0200

    Application

---
+++ Aale und Rolltreppen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aale_und_Rolltreppen;
+using EelsAndEscalators;
+using EelsAndEscalators.Contracts;
+using EelsAndEscalators.States;
+
+namespace Aale_und_Rolltreppen
+{
+    //Nico
+    class Program
+    {
+        //private static IGame game;
+
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Init();
+        }
+    }
+}
