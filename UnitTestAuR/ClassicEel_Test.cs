160c75ae76cce4e10eb08ea66c4ee28568c27b36
INIT\HNI <nicohoermann16@gmail.com>
2018-10-23 13:22:08 +0200

    Neuer Eel Test

--- UnitTestAuR/ClassicEel_Test.cs
+++ UnitTestAuR/ClassicEel_Test.cs
@@ -1,78 +1,74 @@
-﻿//using System;
-//using System.Text;
-//using System.Collections.Generic;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-//using EelsAndEscalators;
-//using EelsAndEscalators.Contracts;
-//using EelsAndEscalators.States;
-//using EelsAndEscalators.ClassicEandE;
-
-//namespace UnitTestAuR
-//{
-//    [TestClass]
-//    public class ClassicEel_Test
-//    {
-//        public Func<IEntity> Creator;
-
-//        int pawnLocationUnderTest;
-//        int eelTopLocationUnderTest;
-
-//        bool result;
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using EelsAndEscalators;
+using EelsAndEscalators.Contracts;
+using EelsAndEscalators.States;
+using EelsAndEscalators.ClassicEandE;
 
 
-//        [TestInitialize]
-//        public void Setup()
-//        {
-//            eelTopLocationUnderTest = 6;
-//            pawnLocationUnderTest = 6;
+namespace UnitTestAuR
+{
+    [TestClass]
+    public class ClassicEel_Test
+    {
+        public Func<IEntity> Creator;
+
+        ClassicPawn _testPawn = new ClassicPawn();
+        //int pawnLocationUnderTest;
+        //int eelTopLocationUnderTest;
+
+        private bool result;
 
 
-//            var mockedPawn = new Mock<IPawn>();
-//            mockedPawn.Setup(m => m.location).
-//                Returns(() => pawnLocationUnderTest);
+        [TestInitialize]
+        public void Setup()
+        {
+            //eelTopLocationUnderTest = 6;
+            _testPawn.location = 6;
+
+            Creator = () => new ClassicEel();
+        }
+        [TestMethod]
+        public void If_Calling_OnSamePositionAs__Return_True_If_Entity_On_Same_Position()
+        {
+
+            var eel = Creator();
+            eel.bottom_location = 3;
+            eel.top_location = 6;
+
+            result = eel.OnSamePositionAs(_testPawn);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void If_Calling_OnSamePositionAs__Return_False_If_Entity_Not_On_Same_Position()
+        {
+            _testPawn.location = 4;
+
+            var eel = Creator();
+            eel.top_location = 6;
+            eel.bottom_location = 3;
+            result = eel.OnSamePositionAs(_testPawn);
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void If_Calling_MovePawn__New_Pawn_Location_Should_Be_Lower_Than_Old_Location()
+        {
 
 
-//            var mockedEel = new Mock<IEntity>();
-//            mockedEel.Setup(m => m.top_location).
-//                Returns(() => eelTopLocationUnderTest);
+            var eel = Creator();
+            eel.top_location = 6;
+            eel.bottom_location = 3;
+            eel.SetPawn(_testPawn);
 
-//            Creator = () => new ClassicEel(mockedPawn.Object);
-//        }
-//        [TestMethod]
-//        public void If_Calling_OnSamePositionAs__Return_True_If_Entity_On_Same_Position()
-//        {
+            result = _testPawn.location < 6 ? true : false;
 
-//            var eel = Creator();
+            Assert.IsTrue(result);
 
-//            result = eel.OnSamePositionAs();
-
-//            Assert.IsTrue(result);
-//        }
-//        [TestMethod]
-//        public void If_Calling_OnSamePositionAs__Return_False_If_Entity_Not_On_Same_Position()
-//        {
-//            pawnLocationUnderTest = 4;
-
-//            var eel = Creator();
-
-//            result = eel.OnSamePositionAs();
-
-//            Assert.IsFalse(result);
-//        }
-//        [TestMethod]
-//        public void If_Calling_MovePawn__New_Pawn_Location_Should_Be_Lower_Than_Old_Location()
-//        {
-
-
-//            var eel = Creator();
-
-//            eel.SetPawn();
-
-//            result = pawnLocationUnderTest < 6 ? true : false;
-
-//            Assert.IsTrue(result);
-
-//        }
-//    }
-//}
+        }
+    }
+}
