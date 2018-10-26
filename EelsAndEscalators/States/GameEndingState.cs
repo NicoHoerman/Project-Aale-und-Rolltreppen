3080864b4460b1a4769b605acc9d42b5cd70c33b
INIT\HNI <nicohoermann16@gmail.com>
2018-10-26 12:15:45 +0200

    neur state

---
+++ EelsAndEscalators/States/GameEndingState.cs
@@ -0,0 +1,24 @@
+﻿using EelsAndEscalators.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EelsAndEscalators.States
+{
+    class GameEndingState :IState
+    {
+        private readonly IGame _game;
+
+        public GameEndingState(IGame game)
+        {
+            _game = game;
+        }
+
+        public void Execute()
+        {
+            Environment.Exit(0);
+        }
+    }
+
+}
+
